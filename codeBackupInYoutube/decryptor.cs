311d30a0e66e38068f344d8fd6f121e249e18d81
Neria Gelman <118425331+Neria-Gelman@users.noreply.github.com>
2023-02-28 17:00:13 +0200

    Add files via upload

---
+++ codeBackupInYoutube/decryptor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+class decryptor
+{
+    static void Main()
+    {
+        string videoFilePath = "example.mp4";
+        string decryptedFilePath = "decrypted.txt";
+
+        byte[] inputData = File.ReadAllBytes(videoFilePath);
+
+        byte[] key = new byte[32];
+        byte[] iv = new byte[16];
+        Array.Copy(inputData, key, 32);
+        Array.Copy(inputData, 32, iv, 0, 16);
+
+        byte[] videoData = new byte[inputData.Length - 32 - 16];
+        Array.Copy(inputData, 32 + 16, videoData, 0, videoData.Length);
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            using (CryptoStream cryptoStream = new CryptoStream(new MemoryStream(videoData), aesAlg.CreateDecryptor(key, iv), CryptoStreamMode.Read))
+            using (StreamReader reader = new StreamReader(cryptoStream))
+            {
+                string outputText = reader.ReadToEnd();
+                File.WriteAllText(decryptedFilePath, outputText);
+            }
+        }
+    }
+}
---
+++ codeBackupInYoutube/encryptor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+class encryptor
+{
+    static void Main()
+    {
+        string inputText = "Hello, World!";
+        string videoFilePath = "example.mp4";
+        string encryptedFilePath = "encrypted.mp4";
+
+        byte[] videoData = File.ReadAllBytes(videoFilePath);
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            byte[] key = aesAlg.Key;
+            byte[] iv = aesAlg.IV;
+
+            using (FileStream encryptedStream = new FileStream(encryptedFilePath, FileMode.Create))
+            using (CryptoStream cryptoStream = new CryptoStream(encryptedStream, aesAlg.CreateEncryptor(key, iv), CryptoStreamMode.Write))
+            using (StreamWriter writer = new StreamWriter(cryptoStream))
+            {
+                writer.Write(inputText);
+            }
+
+            byte[] encryptedData = File.ReadAllBytes(encryptedFilePath);
+
+            // Append the key and IV to the beginning of the video data
+            byte[] outputData = new byte[videoData.Length + key.Length + iv.Length];
+            key.CopyTo(outputData, 0);
+            iv.CopyTo(outputData, key.Length);
+            videoData.CopyTo(outputData, key.Length + iv.Length);
+
+            File.WriteAllBytes(videoFilePath, outputData);
+        }
+    }
+}
